7c6c88aef34dfa9d43a0a485254b63f5b19ead0b
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2018-09-07 07:18:16 +0530

    Modified code to use sensors in a more usable way. Also added ds18x20 and onewire support.

--- boot.py
+++ boot.py
@@ -1,40 +1,59 @@
-# This file is executed on every boot (including wake-boot from deepsleep)
-#import esp
-#esp.osdebug(None)
 import gc
 import webrepl
 import network
 import ujson as json
+import uos as os
 
 webrepl.start()
 ap_if = network.WLAN(network.AP_IF)
-if not ap_if.active():
-    ap_if.active(True)
+ap_setup_file = "accesspoint.json"
 
-with open("accesspoint.json", "r") as f:
-    access_point = json.load(f)
-    ap_if.config(
-        essid=access_point["essid"], 
-        channel=access_point["channel"],
-        password=access_point["password"])
+if ap_setup_file in os.listdir():
+    # Start the access point.
+    if not ap_if.active():
+        ap_if.active(True)
+    with open(ap_setup_file, "r") as f:
+        access_point = json.load(f)
+        ap_if.config(
+            essid=access_point["essid"], 
+            channel=access_point["channel"],
+            password=access_point["password"])
+else:
+    if ap_if.active():
+        ap_if.active(False)
 
 sta_if = network.WLAN(network.STA_IF)
-if not sta_if.active():
-    sta_if.active(True)
+sta_setup_file = "networks.json"
+if sta_setup_file in os.listdir():
+    with open(sta_setup_file, "r") as f:
+        network_data = json.load(f)
+    if not sta_if.active():
+        sta_if.active(True)
+    available_networks = sta_if.scan()
+    for network in available_networks:
+        network_name = network[0].decode()
+        if not sta_if.isconnected():
+            for network in network_data:
+                if network_name == network["essid"]:
+                    try:
+                        network_config = network["static_ip"]
+                        network_config = tuple(network_config)
+                    except KeyError:
+                        pass
+                    else:
+                        sta_if.ifconfig(network_config)
+                    sta_if.connect(network_name, network["password"])
+        else:
+            break
+    identity_file = "identity.json"
+    if identity_file in os.listdir():
+        with open(identity_file, "r") as f:
+            identity = json.load(f)
+        location = identity.get("location")
+        if location is not None:
 
-
-available_networks = sta_if.scan()
-with open("networks.json", "r") as f:
-    network_data = json.load(f)
-for network in available_networks:
-    network_name = network[0].decode()
-    if not sta_if.isconnected():
-        for network in network_data:
-            if network_name == network["essid"]:
-                sta_if.connect(network_name, network["password"])
-    else:
-        break
-with open("identity.json", "r") as f:
-    identity = json.load(f)
-sta_if.config(dhcp_hostname=identity)
+            sta_if.config(dhcp_hostname=location.lower().replace(" ","_"))
+else:
+    if sta_if.active():
+        sta_if.active(True)
 gc.collect()
--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-# main file.
-# This file is run after boot.py.
 import usocket as socket
 import ujson as json
-import machine
-import dht
+import machine, time
 
-CONTENT_JSON = b"""HTTP/1.1 200 OK
+"""
+http 1.1 response rules
+    A Status-line
+
+    Zero or more header (General|Response|Entity) fields followed by CRLF
+
+    An empty line (i.e., a line with nothing preceding the CRLF) 
+    indicating the end of the header fields
+
+    Optionally a message-body
+
+"""
+
+CONTENT_JSON = """HTTP/1.1 200 OK
 Access-Control-Allow-Origin: *
 Response Status Code -> 200 OK
 Access-Control-Allow-Methods: GET, POST
@@ -16,7 +26,7 @@ Response Body:
 %s
 """
 
-CONTENT_HTML = b"""HTTP/1.1 200 OK
+CONTENT_HTML = """HTTP/1.1 200 OK
 Access-Control-Allow-Origin: *
 Response Status Code -> 200 OK
 Access-Control-Allow-Methods: GET, POST
@@ -27,96 +37,149 @@ Response Body:
 %s
 """
 
-
-def parse_req(myrequest):
-    """Function to pass the url and return the path"""
-    myrequest = str(myrequest)
-    items = myrequest.strip().split('\r\n')
+def parse_req(my_request):
+    """Function to pass the url and return the path
+    
+    url = /write/PIN/on
+    """
+    my_request = str(my_request)
+    request_items = my_request.strip().split('\r\n')
+    url = None
     path = ""
     param_pairs = []
-    for item in items:
-        if 'GET' in item:
-            adr = item.split()[1]
-            if '?' in adr: 
-                adr, params = adr.split('?', 1)
+    for item in request_items:
+        if 'GET' in item.upper():
+            url = item.split()[1]
+            if '?' in url: 
+                url, params = url.split('?', 1)
             else:
                 params = []
-            adr = adr.split('/')
+            url = url.split('/')
+            url = [x.lower() for x in url if x != ""]
             if params:
                 param_pairs = [p.split('=') for p in  params.split('&')]
             else:
                 param_pairs = []
+        # TODO: Implement other HTTP commands.
     param_dict = {key.lower():value.lower() for [key, value] in param_pairs}
-    return adr, param_dict
+    return url, param_dict
 
-def exec_req(adr, param_dict):
-    """Function to execute the request"""
-    print("URL:", adr, param_dict)
-    if adr[1] == 'write':
+def exec_req(url, param_dict):
+    """Function to execute the request
+    This function executes the parsed request.
+
+    Arguments:
+        url: list, separated by /.
+            So if a user comes to http://example.domain.com/read/1
+            this argument is ["read","1"]
+        
+        param_dict: dictionary, of the parameters that are sent to the server.
+
+        
+    """
+    print("URL:", url, param_dict)
+    if len(url) == 0:
+        with open("identity.json", "r") as f:
+            identity = json.load(f)
+        return """<html>
+                <head>
+                    <title>NodeMCU</title>
+                </head>
+                <body>
+                    <b>Location: {}</b>
+                </body>
+            </html>
+            """.format(identity["location"])
+    elif url[0] == 'write':
+        pin_id = int(url[1])
         try:
-            pinid = int(adr[2])
-            pin = machine.Pin(pinid, machine.Pin.OUT)
+            pin = machine.Pin(pin_id, machine.Pin.OUT)
         except:
-            return {"status": "Error"}
-        if adr[3] == 'on':
-            pin.on()
-            return {"status": "Pin {} is on".format(str(pinid))}
-        elif adr[3] == 'off':
-            pin.off()
-            return {"status": "Pin {} is off".format(str(pinid))}
-    elif adr[1] == 'read':
+            return {"status": "Error, pin value {} is inaccessible".format(pin_id)}
+        else:
+            if url[2] == 'on':
+                pin.on()
+                return {"status": "Pin {} is on".format(str(pin_id))}
+            elif url[2] == 'off':
+                pin.off()
+                return {"status": "Pin {} is off".format(str(pin_id))}
+            else:
+                return {"status": "{} is not a valid input for pin {} status".format(url[3], pin_id)}
+    elif url[0] == 'read':
         try:
-            pinid = int(adr[2])
-
+            pin_id = int(url[1])
             if "pull" in param_dict.keys():
                 pull = param_dict["pull"]
             else:
                 pull = None
-
             if pull == "up":
                 pull = machine.Pin.PULL_UP
             elif pull == "down":
                 pull = machine.Pin.PULL_DOWN
-            pin = machine.Pin(pinid, machine.Pin.IN, pull) #todo
+            pin = machine.Pin(pin_id, machine.Pin.IN, pull) #todo
             return {"value": pin.value()}
         except:
             return {
                 "Status": "Error"
                 }
-    elif adr[1] == "measure":
-        try:
-            pinid = int(adr[2])
-            d = dht.DHT11(machine.Pin(pinid))
-            d.measure()
-            return {"temperature": d.temperature(),  "humidity": d.humidity()}
-        except:
-            return {"Status": "Error"}
-    elif adr[1] == "whoami":
+    elif url[0] == "measure":
+        pin_id = int(url[1])
+        measurement_type = param_dict.get("type")
+        if measurement_type is None:
+            return {"status": "measurement type is required in the query arguments!"}
+        elif measurement_type.lower() == "dht11":
+            # /measure/PIN_ID?type="dht11"
+            try:
+                import dht
+                d = dht.DHT11(machine.Pin(pin_id))
+                d.measure()
+                return {"temperature": d.temperature(),  "humidity": d.humidity()}
+            except:
+                return {"Status": "Error"}
+        elif measurement_type.lower() == "ds18b20":
+            # /measure/PIN_ID?type=ds18b20
+            import onewire, ds18x20
+            temperatures = []
+            try:
+                dat = machine.Pin(pin_id)
+                ds = ds18x20.DS18X20(onewire.OneWire(dat))
+                roms = ds.scan()
+                ds.convert_temp()
+                time.sleep_ms(750)
+                temperatures = []
+                for rom in roms:
+                    temperature = ds.read_temp(rom)
+                    address = hex(int.from_bytes(rom, "little"))
+                    temperatures.append({"id":address, "temperature": round(temperature, 4)})
+            except Exception as e:
+                return {
+                    "status":"{} error while attempting to measure ds18b20 temperature!".format(repr(e))
+                    }
+            return {
+                "temperatures": [
+                    temperatures
+                ],
+                "unit": "celsius"}
+        else:
+            return {
+                "status": "{} measurement not implemented.".format(measurement_type.lower())
+                }
+    elif url[0] == "whoami":
         with open("identity.json", "r") as f:
             identity = json.load(f)
         return identity
-    elif adr[1] == "":
-        d = dht.DHT11(machine.Pin(3))
-        d.measure()
-        with open("identity.json", "r") as f:
-            identity = json.load(f)
-        return """<html><head><title>Temperature Monitor Page</title></head>
-        <body>
-        <b>Temperature: {} °C</b><br/>
-        <b>Humidity: {} %</b><br/>
-        <b>Location: {}</b><br/></body></html>""".format(d.temperature(), d.humidity(), identity["location"])
-
-    return {"Status": "No Action"}
+    else:
+        return {"Status": "No Action"}
     
 
 def main(micropython_optimize=False):
     s = socket.socket()
 
-    # Binding to all interfaces - server will be accessible to other hosts!
+    # Binding to all interfaces - 
+    # server will be accessible to other hosts!
     ai = socket.getaddrinfo("0.0.0.0", 80)
     print("Bind address info:", ai)
     addr = ai[0][-1]
-
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind(addr)
     s.listen(5)
@@ -129,25 +192,24 @@ def main(micropython_optimize=False):
         client_addr = res[1]
         print("Client address:", client_addr)
         print("Client socket:", client_sock)
-
         if not micropython_optimize:
             client_stream = client_sock.makefile("rwb")
         else:
             client_stream = client_sock
 
-        print("Request:")
         req = client_stream.readline()
-        print(req)
-        myrequest = req
+        print("Incoming Request: {}" .format(req))
+        my_request = req
+        print("Headers:")
         while True:
+            # Read request headers
             h = client_stream.readline()
             if h == b"" or h == b"\r\n":
                 break
             print(h)
-            myrequest = myrequest + h
-        print("----------------------")
-        print(myrequest)
-        url, param_dict = parse_req(myrequest)
+            my_request = my_request + h
+        print(my_request)
+        url, param_dict = parse_req(my_request)
         #print(url)
         out = exec_req(url, param_dict)
         #out = "URL: {}, PARAM: {}".format(str(url), str(param_dict))
@@ -155,11 +217,14 @@ def main(micropython_optimize=False):
             response = CONTENT_JSON % out
         else:
             response = CONTENT_HTML % out
+        # HTTP protocol needs CRLF in the response.
+        if "\r\n" not in response:
+            response=response.replace("\n","\r\n")
+        response = response.encode("bytes")
         client_stream.write(response)
-
         client_stream.close()
+        print(response)
         if not micropython_optimize:
             client_sock.close()
-        print()
 
 main()
